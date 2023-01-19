4e3b2a843c826fa2039062e7cc3fed7a54005619
Bartosz Ciereszyński <80280@student.pb.edu.pl>
2023-01-19 22:57:04 +0100

    QuizzActivity issues and warnings review

--- app/src/main/java/com/example/quizzapp/QuizzActivity.java
+++ app/src/main/java/com/example/quizzapp/QuizzActivity.java
@@ -3,7 +3,6 @@ package com.example.quizzapp;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
@@ -29,29 +28,22 @@ public class QuizzActivity extends AppCompatActivity implements Observer {
     private int selectedItem;
     private TextView qText;
     private Button next;
-    private boolean checkAnswerCorrectness(String userAnswer){
-        int resultMessageId = 0;
-        boolean result = quizz.getCurrentQuestion().TryAnswer(userAnswer);
-        if(quizz.isContinuePossible())
-            next.setEnabled(true);
-        else{
-            next.setEnabled(false);
-        }
-        if(result){
-            resultMessageId = R.string.correct_answer;
-            Toast.makeText(this,resultMessageId,Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        else{
-            return false;
-        }
 
-    }
-    private void setNewQuestion(){
+    private void setNewQuestion() {
         quizz.generateNewQuestion();
         qText.setText(quizz.getCurrentQuestion().getGoodAnswer().getContent());
         adapter.setPossibleAnswers(quizz.getCurrentQuestion().getPossibleAnswers());
         selectedItem = -1;
+    }
+
+    private void checkAnswerCorrectness(String userAnswer) {
+        boolean result = quizz.getCurrentQuestion().TryAnswer(userAnswer);
+        next.setEnabled(quizz.isContinuePossible());
+
+        if (result) {
+            Toast.makeText(this,  R.string.correct_answer, Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     @Override
@@ -76,34 +68,22 @@ public class QuizzActivity extends AppCompatActivity implements Observer {
 
         quizz = new EN_PL_Quizz(this, new Hard());
 
-
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setNewQuestion();
-            }
-        });
-
+        next.setOnClickListener(v -> setNewQuestion());
 
     }
-    private class AnswerHolder extends RecyclerView.ViewHolder{
-        String answer;
-        private TextView contentTextView;
-        private Button answerButton;
-        public AnswerHolder(LayoutInflater inflater, ViewGroup parent ){
-            super(inflater.inflate(R.layout.answer_list_item, parent, false));
-            contentTextView= itemView.findViewById(R.id.answer_content);
-            answerButton= itemView.findViewById(R.id.answer_content);
-            answerButton.setOnClickListener((v)->{
-                int position = getBindingAdapterPosition();
-                if (position == RecyclerView.NO_POSITION || position == selectedItem) {
-                    return;
-                }
-                checkAnswerCorrectness(answer);
-                answerButton.setEnabled(false);
 
+    private class AnswerHolder extends RecyclerView.ViewHolder {
+
+        String answer;
+        private final Button answerButton;
+
+        public AnswerHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.answer_list_item, parent, false));
+            answerButton = itemView.findViewById(R.id.answer_content);
+            answerButton.setOnClickListener(v -> {
+                checkAnswerCorrectness(answer);
                 int previousSelected = selectedItem;
-                selectedItem = position;// Check item.
+                selectedItem = getBindingAdapterPosition();// Check item.
 
                 if (previousSelected != -1) {
                     adapter.notifyItemChanged(previousSelected);// Deselected the previous item.
@@ -111,49 +91,47 @@ public class QuizzActivity extends AppCompatActivity implements Observer {
                 adapter.notifyItemChanged(selectedItem);// Select the current item.
             });
         }
-        public void bind(String answer, boolean choosen){
+
+        public void bind(String answer, boolean chosen) {
             this.answer = answer;
-            contentTextView.setText(answer);
-            if(choosen)
-                answerButton.setEnabled(false);
-            else
-                answerButton.setEnabled(true);
+            answerButton.setText(answer);
+            answerButton.setEnabled(!chosen);
         }
 
     }
 
-    private class AnswerAdapter extends RecyclerView.Adapter<AnswerHolder>{
+    private class AnswerAdapter extends RecyclerView.Adapter<AnswerHolder> {
         private List<String> possibleAnswers;
 
         @NonNull
         @Override
-        public AnswerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType ){
+        public AnswerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             return new AnswerHolder(getLayoutInflater(), parent);
         }
+
         @Override
-        public void onBindViewHolder(@NonNull AnswerHolder holder, int position){
-            if(possibleAnswers!= null){
+        public void onBindViewHolder(@NonNull AnswerHolder holder, int position) {
+            if (possibleAnswers != null) {
                 String answer = possibleAnswers.get(position);
-                Boolean choosen = position == selectedItem;
-                holder.bind(answer, choosen);
-            }
-            else{
+                boolean chosen = position == selectedItem;
+                holder.bind(answer, chosen);
+            } else {
                 Log.d("MainActivity", "Answers ERROR");
             }
 
         }
-        @Override
-        public int getItemCount(){
 
-            if(possibleAnswers !=null) {
+        @Override
+        public int getItemCount() {
+
+            if (possibleAnswers != null) {
                 return possibleAnswers.size();
-            }
-            else{
+            } else {
                 return 0;
             }
         }
 
-        public void setPossibleAnswers(List<String> possibleAnswers){
+        public void setPossibleAnswers(List<String> possibleAnswers) {
             this.possibleAnswers = possibleAnswers;
             notifyDataSetChanged();
         }
