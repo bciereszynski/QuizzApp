5d53fe9164617d686eba43cfbd159175e9305b04
Bluefish5 <60304472+Bluefish5@users.noreply.github.com>
2023-01-20 04:45:04 +0100

    crud work in progres
    Nie wiem, nie skończyłem cruda bo nie wiem jak rozwiązać to ze jak dodaje słowo to activiti wie do którego view dodac(do eng czy do pl)

--- app/src/main/java/com/example/quizzapp/AddWordActivity.java
+++ app/src/main/java/com/example/quizzapp/AddWordActivity.java
@@ -1,14 +1,25 @@
 package com.example.quizzapp;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddWordActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    public static final String EXTRA_WORD = "pb.edu.pl.EXTRA_WORD";
+    public static final String EXTRA_TRANSLATION = "pb.edu.pl.EXTRA_TRANSLATION";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +31,40 @@ public class AddWordActivity extends AppCompatActivity implements AdapterView.On
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+
+        EditText addWordText = findViewById(R.id.addWordText);
+        EditText addTranslationText = findViewById(R.id.addTranslationText);
+
+        Button button = findViewById(R.id.addWordSubmit);
+        button.setOnClickListener(view -> {
+            Intent replayIntent = new Intent();
+            if(TextUtils.isEmpty(addWordText.getText())
+                    || TextUtils.isEmpty(addTranslationText.getText())){
+                setResult(RESULT_CANCELED,replayIntent);
+
+            }else{
+                String word = addWordText.getText().toString();
+                String traslation = addTranslationText.getText().toString();
+                replayIntent.putExtra(EXTRA_WORD,word);
+                replayIntent.putExtra(EXTRA_TRANSLATION,traslation);
+
+
+                if(spinner.getSelectedItem().toString()=="Polski -> Angielski"){
+                    setResult(2,replayIntent);
+                }
+                if(spinner.getSelectedItem().toString()=="English -> Polish"){
+                    setResult(3,replayIntent);
+                }
+
+            }
+            finish();
+
+
+
+        });
     }
+
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
--- app/src/main/java/com/example/quizzapp/EditWordActivity.java
+++ app/src/main/java/com/example/quizzapp/EditWordActivity.java
@@ -1,17 +1,25 @@
 package com.example.quizzapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.quizzapp.database.EnglishWordViewModel;
+import com.example.quizzapp.database.PolishWordViewModel;
 
 public class EditWordActivity extends AppCompatActivity {
 
-    public static final String EXTRA_EDIT_WORD = "pb.edu.pl.EDIT_WORD";
-    public static final String EXTRA_EDIT_TRANSLATION = "pb.edu.pl.EDIT_TRANSLATION";
+
+    public static final String EXTRA_WORD = "pb.edu.pl.EXTRA_WORD";
+    public static final String EXTRA_TRANSLATION = "pb.edu.pl.EXTRA_TRANSLATION";
+    PolishWordViewModel polishWordViewModel;
+    EnglishWordViewModel englishWordViewModel;
     private EditText editWordText,editTranslationText;
 
     @Override
@@ -19,22 +27,40 @@ public class EditWordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_word);
 
+        polishWordViewModel = new ViewModelProvider(this).get(PolishWordViewModel.class);
+        englishWordViewModel = new ViewModelProvider(this).get(EnglishWordViewModel.class);
+
+        Intent intent = getIntent();
+        String wordIn = intent.getStringExtra(CrudActivity.EXTRA_WORD);
+        String translationIn = intent.getStringExtra(CrudActivity.EXTRA_TRANSLATION);
+
+
         editWordText = findViewById(R.id.editWordText);
         editTranslationText = findViewById(R.id.editTranslationText);
+
+        editWordText.setText(wordIn);
+        editTranslationText.setText(translationIn);
+
+
 
         Button button = findViewById(R.id.editSubmit);
         button.setOnClickListener(view -> {
             Intent replayIntent = new Intent();
-            if(TextUtils.isEmpty(editWordText.getText())||TextUtils.isEmpty(editTranslationText.getText())){
+            if(TextUtils.isEmpty(editWordText.getText())
+            || TextUtils.isEmpty(editTranslationText.getText())){
                 setResult(RESULT_CANCELED,replayIntent);
-            }else {
+
+            }else{
                 String word = editWordText.getText().toString();
-                replayIntent.putExtra(EXTRA_EDIT_WORD, word);
-                String translation = editTranslationText.getText().toString();
-                replayIntent.putExtra(EXTRA_EDIT_TRANSLATION,translation);
-                setResult(RESULT_OK,replayIntent);
+                String traslation = editTranslationText.getText().toString();
+                replayIntent.putExtra(EXTRA_WORD,word);
+                replayIntent.putExtra(EXTRA_TRANSLATION,traslation);
+                setResult(200,replayIntent);
             }
             finish();
+
+
+
         });
     }
 }
