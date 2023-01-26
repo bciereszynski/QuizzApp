01aef10d699f292bcd275e8862db50c0d49aca9c
Woiti01 <zajwoy01@gmail.com>
2023-01-26 15:56:54 +0100

    NOtifications

--- app/src/main/java/com/example/quizzapp/QuizzActivity.java
+++ app/src/main/java/com/example/quizzapp/QuizzActivity.java
@@ -2,10 +2,15 @@ package com.example.quizzapp;
 
 import static android.view.View.GONE;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -17,12 +22,35 @@ public class QuizzActivity extends AppCompatActivity {
     private FragmentManager fragmentManager;
     private FragmentQuizz fragmentq;
     private Button next;
+    private NotificationManagerCompat notificationManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
+
+        //Notification
+        String CHANNEL_ID = "5";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Notification";
+            String description = "Description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification_icon)
+                .setContentTitle("QuizzApp")
+                .setContentText("W trakcie quizzu...")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
 
         //binding
         next = findViewById(R.id.quizz_button);
@@ -45,5 +73,11 @@ public class QuizzActivity extends AppCompatActivity {
                     .commit();
             next.setVisibility(GONE);
         });
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        notificationManager.cancel(1);
     }
 }
